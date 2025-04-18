f0c9a7e34b82b0a11b18ca3d0dd12e7c29e70399
huiqiang.shang <huiqiang.shang@shiyak.com>
2025-04-18 14:20:39 +0800

    refactor: enhance consul service registration and deregistration with context logging

--- server/consul.go
+++ server/consul.go
@@ -2,7 +2,6 @@ package server
 
 import (
 	"context"
-	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
@@ -28,27 +27,27 @@ func register(conf *config.DeployConfig) {
 		c := make(chan os.Signal, 1)
 		signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 		sig := <-c
-		if err := deregeisterService(conf.Id); err != nil {
-			log.Error(ctx, "de", zap.Error(err))
+		if err := deregeisterService(ctx, conf.Id); err != nil {
+			log.Error(ctx, "deregister service err", zap.Error(err))
 		}
 		log.Info(ctx, "", zap.Any("sig", sig))
 
 		os.Exit(0)
 	}()
 
-	if err := registerService(conf.Id, conf.AppName, conf.Port, conf.Addr, conf.Consul.Addr); err != nil {
-		log.Error(ctx, "", zap.Error(err))
+	if err := registerService(ctx, conf.Id, conf.AppName, conf.Port, conf.Addr, conf.Consul.Addr); err != nil {
+		log.Error(ctx, "register service err", zap.Error(err))
 	}
 }
 
-func registerService(id string, name string, port int, serviceAddr string, consulAddr string) error {
+func registerService(ctx context.Context, id string, name string, port int, serviceAddr string, consulAddr string) error {
 	consulConfig := api.DefaultConfig()
 
 	consulConfig.Address = consulAddr
 
 	consulClient, err := api.NewClient(consulConfig)
 	if err != nil {
-		fmt.Println("api.NewClient err:", err)
+		log.Error(ctx, "new consul client err", zap.Error(err))
 		return err
 	}
 
@@ -59,7 +58,7 @@ func registerService(id string, name string, port int, serviceAddr string, consu
 		Address: serviceAddr,
 		Port:    port,
 		Check: &api.AgentServiceCheck{
-			HTTP:     "http://host.docker.internal:8888/health",
+			HTTP:     "http://" + serviceAddr + ":8888/health",
 			Timeout:  "1s",
 			Interval: "5s",
 		},
@@ -67,23 +66,23 @@ func registerService(id string, name string, port int, serviceAddr string, consu
 
 	err = consulClient.Agent().ServiceRegister(&registerService)
 	if err != nil {
-		fmt.Println("consulClient.Agent().ServiceRegister err:", err)
+		log.Error(ctx, "consul register service err", zap.Error(err))
 		return err
 	}
 	return nil
 }
 
-func deregeisterService(id string) error {
+func deregeisterService(ctx context.Context, id string) error {
 	consulConfig := api.DefaultConfig()
 	consulClient, err := api.NewClient(consulConfig)
 	if err != nil {
-		fmt.Println("api.NewClient err:", err)
+		log.Error(ctx, "api.NewClient err", zap.Error(err))
 		return err
 	}
 
 	err = consulClient.Agent().ServiceDeregister(id)
 	if err != nil {
-		fmt.Println("consulClient.Agent().ServiceDeregister err:", err)
+		log.Error(ctx, "consulClient.Agent().ServiceDeregister err", zap.Error(err))
 		return err
 	}
 	return nil
